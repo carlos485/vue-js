3da16bb701e85e1dcb249de457980b14cb18d6da
carlos <carlos@gmail.com>
2020-01-29 11:40:57 -0500

    primer commit

---
+++ 01.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hola Mundo Con Vue JS',
+        colores: [
+            {nombre: 'rojo', cantidad: 1},
+            {nombre: 'azul', cantidad: 0},
+            {nombre: 'verde', cantidad: 15}
+        ]
+    }
+})
